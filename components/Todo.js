fb09472a3211d047f1cad94250021f5e4ca6c97b
Eric Malone <Malone1913@gmail.com>
2026-06-26 15:51:03 -0500

    Refactor validation and popup behavior

---
+++ components/Todo.js
@@ -0,0 +1,54 @@
+export default class Todo {
+  constructor(data, selector) {
+    this._name = data.name;
+    this._id = data.id;
+    this._completed = data.completed;
+    this._date = data.date;
+    this._selector = selector;
+  }
+
+  _getTemplate() {
+    return document
+      .querySelector(this._selector)
+      .content.querySelector(".todo")
+      .cloneNode(true);
+  }
+
+  _setEventListeners() {
+    this._todoDeleteButton.addEventListener("click", () => {
+      this._todoElement.remove();
+    });
+  }
+
+  _setDate() {
+    const dueDate = new Date(this._date);
+
+    if (!isNaN(dueDate)) {
+      this._todoDate.textContent = `Due: ${dueDate.toLocaleString("en-US", {
+        year: "numeric",
+        month: "short",
+        day: "numeric",
+      })}`;
+    }
+  }
+
+  getView() {
+    this._todoElement = this._getTemplate();
+    this._todoName = this._todoElement.querySelector(".todo__name");
+    this._todoCheckbox = this._todoElement.querySelector(".todo__completed");
+    this._todoLabel = this._todoElement.querySelector(".todo__label");
+    this._todoDate = this._todoElement.querySelector(".todo__date");
+    this._todoDeleteButton =
+      this._todoElement.querySelector(".todo__delete-btn");
+
+    this._todoName.textContent = this._name;
+    this._todoCheckbox.checked = this._completed;
+    this._todoCheckbox.id = `todo-${this._id}`;
+    this._todoLabel.setAttribute("for", `todo-${this._id}`);
+
+    this._setDate();
+    this._setEventListeners();
+
+    return this._todoElement;
+  }
+}
